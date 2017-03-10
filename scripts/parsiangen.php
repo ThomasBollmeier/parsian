7f5bfaba707d7bb06b8c0a5c39063a9e405befc6
Thomas Bollmeier <entwickler@tbollmeier.de>
2017-03-10 17:53:30 +0100

    Autoloading for generator script

--- scripts/parsiangen.php
+++ scripts/parsiangen.php
@@ -16,7 +16,31 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-require __DIR__ . '/../vendor/autoload.php';
+
+spl_autoload_register(function ($qualifiedName) {
+
+    $nameParts = explode("\\", $qualifiedName);
+
+    if ((count($nameParts) < 2 ||
+        $nameParts[0] !== "tbollmeier" ||
+        $nameParts[1] !== "parsian")) {
+
+        return;
+
+    }
+
+    array_shift($nameParts);
+    array_shift($nameParts);
+
+    $path = __DIR__ .  DIRECTORY_SEPARATOR . "..";
+    $path .= DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR;
+    $path .= implode(DIRECTORY_SEPARATOR, $nameParts) . ".php";
+    $path = realpath($path);
+
+    require $path;
+
+});
+
 
 use tbollmeier\parsian\codegen\Api;
 
